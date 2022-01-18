7ef0e86d0d6246e0a1c61e9898c290c9f91fb784
egri-richard <egri.dani.r@gmail.com>
2022-01-18 14:36:24 +0100

    cleanup + added table to display statues + created basic fetch to get statues

--- statues/src/App.vue
+++ statues/src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <table>
+      <tr>
+        <th>Person</th>
+        <th>Height</th>
+        <th>Price</th>
+        <th>Operations</th>
+      </tr>
+      <tr v-for="s in statues" :key="s.id">
+        <td>{{ s.person }}</td>
+        <td>{{ s.height }}</td>
+        <td>{{ s.price }}</td>
+        <td>
+          <button>Delete</button>
+          <button>Edit</button>
+        </td>
+      </tr>
+      <tr>
+
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return { 
+      statues: Array
+    }
+  },
+  methods: {
+    async getStatues() {
+      let response = await fetch("http://127.0.0.1:8000/api/statues")
+      let data = await response.json()
+      this.statues = data
+    }
+  },
+  mounted() {
+    this.getStatues()
   }
 }
 </script>
